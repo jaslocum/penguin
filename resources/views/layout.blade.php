a4796d89ac5db6bffb5ff4d5d0bf029bcc21c96a
jaslocum <jack@surfacefinishtech.com>
2016-01-19 09:44:56 -0500

    added home page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Penguin</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 1/19/2016
+ * Time: 8:57 AM
+ */
+@extends('layout')
+
+
+
+@section('content')
+
+    <h1>Penguin</h1>
+
+@stop
