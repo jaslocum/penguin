31a9b1031ca0563626c460160524222021bcca09
jaslocum <jack@surfacefinishtech.com>
2016-01-27 07:58:40 -0500

    cleaned up home page a bit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,6 +5,12 @@
         <div class="container">
             <h1>Image Server</h1>
             <p>version 0.1</p>
+            <br>
+            <br>
+            <p>Usage:</p>
+            <br>
+            <p>penguin/{category_id}/{category_rec_id}</p>
+            <p></p>
         </div>
     </div>
 @stop
