c752a87ceb7a31b912ab290e243b0573f8c19a81
jaslocum <jack@surfacefinishtech.com>
2016-01-29 06:17:26 -0500

    added beginning dropzone functionality

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,15 @@
             <br>
             <p>Usage:</p>
             <br>
-            <p>penguin/{category}/{category_rec_id}</p>
+            <p>Web interface to maintain images in a category:<p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;penguin/{category}/{category_rec_id}</p>
+            <br>
+            <p>Return first the image for a category:<p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;penguin/{category}/{category_rec_id}/image</p>
+            <p></p>
+            <br>
+            <p>Web interface to create categories for images:<p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;penguin/categories/create </p>
             <p></p>
         </div>
     </div>
