fe15c2e8b756d2ecad8dc4dc233fc9125d73ed9a
jaslocum <jack@surfacefinishtech.com>
2016-03-09 09:08:08 -0500

    cleanup

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,15 +8,15 @@
             <img src="{{Request::root()}}/1" alt="penguin with attitude" height="80" width="56">
             <h3>Usage:</h3>
             <br>
-            <p>Web interface to edit|create a {category}&{key} bucket for an image bucket:<p>
+            <p>Web interface to edit|create a {category}/{key} bucket or an {image} bucket:<p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{Request::root()}}/{category}/{key}/edit[?description={description}]</p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{Request::root()}}/{image}/edit[?description={description}]</p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{Request::root()}}/create[?description={description}]</p>
             <br>
-            <p>Return first image in a {category}&{key} bucket:<p>
+            <p>Return first image in a {category}/{key} bucket:<p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{Request::root()}}/{category}/{key}</p>
             <br>
-            <p>Return an image for a {filename} in a {category}&{key} bucket:<p>
+            <p>Return an image for a {filename} in a {category}/{key} bucket:<p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{Request::root()}}/{category}/{key}/{filename}</p>
             <br>
             <p>Return an {image}:<p>
