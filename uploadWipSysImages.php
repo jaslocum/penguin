b4cb9b460ec43560b767861d27a3a28cc36071f6
jaslocum <jack@surfacefinishtech.com>
2016-03-09 07:39:11 -0500

    revised uploadWipSysImages to upload eastide images

--- uploadWipSysImages.php
+++ uploadWipSysImages.php
@@ -23,6 +23,167 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+//upload Eastside active work order images
+
+$sql =
+    "
+      SELECT
+        WORKORDR,
+        eoWorkorder.CUSTCODE,
+        eoWorkorder.PROCNAME,
+        eoWorkorder.PARTNUM,
+        eoWorkorder.PartID AS partID,
+        eoWorkorder.ImageID AS WoImageID,
+        Part.ImageID AS PtImageID
+      FROM
+        eoWorkorder
+      JOIN Part Where eoWorkorder.PartID = Part.ID
+      ORDER BY WORKORDR DESC
+    ";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+        $workorder = $row["WORKORDR"];
+        $WoImageId = $row["WoImageID"];
+        $PartID    = $row["partID"];
+        $PtImageId = $row["PtImageID"];
+        $custCode  = $row["CUSTCODE"];
+        $partNum   = $row["PARTNUM"];
+        $process   = $row["PROCNAME"];
+
+        $fileName = "$WoImageId.jpg";
+        $filePath = $path.$fileName;
+        $uri = $url_base."eowoimg/$workorder";
+        $description = "$custCode, $process, $partNum";
+
+        if ($WoImageId!=$PtImageId and $WoImageId>0) {
+
+            //Open file as stream to upload
+            $body = fopen($filePath, 'r');
+
+            if ($body) {
+                $resultPost = $client->post($uri, [
+                    'cookies' => $jar,
+                    'multipart' => [
+                        [
+                            'name' => 'description',
+                            'contents' => $description,
+                        ],
+                        [
+                            'name' => '_token',
+                            'contents' => $session_token['_token'],
+                        ],
+                        [
+                            'name' => "file",
+                            'contents' => $body,
+                            'filename' => "$WoImageId.jpg",
+                            'type' => $mime,
+                        ],
+                    ]
+                ]);
+
+                $image_id = $resultPost->getHeaders()['image_id'][0];
+                $file_path = $resultPost->getHeaders()['file_path'][0];
+                echo "WorkOrder: $workorder, WoPartID: $PartID, filePath: $filePath, mime: $mime, image_id:$image_id, file_path: $file_path\r\n";
+            }
+        }
+    }
+
+} else {
+    echo "0 results\r\n";
+}
+
+//upload Eastside history work order images
+
+$sql =
+    "
+      SELECT
+        WORKORDR,
+        eoHistory.CUSTCODE,
+        eoHistory.PROCNAME,
+        eoHistory.PARTNUM,
+        eoHistory.PartID AS partID,
+        eoHistory.ImageID AS WoImageID,
+        Part.ImageID AS PtImageID
+      FROM
+        eoHistory
+      JOIN Part Where eoHistory.PartID = Part.ID
+      ORDER BY WORKORDR DESC
+    ";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+        $workorder = $row["WORKORDR"];
+        $WoImageId = $row["WoImageID"];
+        $PartID    = $row["partID"];
+        $PtImageId = $row["PtImageID"];
+        $custCode  = $row["CUSTCODE"];
+        $partNum   = $row["PARTNUM"];
+        $process   = $row["PROCNAME"];
+
+        $fileName = "$WoImageId.jpg";
+        $filePath = $path.$fileName;
+        $uri = $url_base."woimg/$workorder";
+        $description = "$custCode, $process, $partNum";
+
+        if ($WoImageId!=$PtImageId and $WoImageId>0) {
+
+            //Open file as stream to upload
+            $body = fopen($filePath, 'r');
+
+            try {
+                if ($body) {
+                    $resultPost = $client->post($uri, [
+                        'cookies' => $jar,
+                        'multipart' => [
+                            [
+                                'name' => 'description',
+                                'contents' => $description,
+                            ],
+                            [
+                                'name' => '_token',
+                                'contents' => $session_token['_token'],
+                            ],
+                            [
+                                'name' => "file",
+                                'contents' => $body,
+                                'filename' => "$WoImageId.jpg",
+                                'type' => $mime,
+                            ],
+                        ]
+                    ]);
+
+                    $image_id = $resultPost->getHeaders()['image_id'][0];
+                    $file_path = $resultPost->getHeaders()['file_path'][0];
+                    echo "History: $workorder, WoPartID: $PartID, filePath: $filePath, mime: $mime, image_id:$image_id, file_path: $file_path\r\n";
+                }
+            } catch (Exception $e) {
+                /*** show the error message ***/
+                echo "\r\n";
+                var_dump($e->getResponse()->getBody()->getContents());
+                echo "\r\n";
+
+                init_session($url_base, $client, $jar, $session_token);
+
+            }
+        }
+    }
+
+} else {
+    echo "0 results\r\n";
+}
+
+//upload Surface active work order images
+
 $sql =
     "
       SELECT
@@ -94,6 +255,8 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results\r\n";
 }
+
+//upload Surface history work order images
 
 $sql =
     "
@@ -177,6 +340,8 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results\r\n";
 }
+
+//upload Surface part images
 
 $sql =
     "
